d4d886352109d967b9e0807407d6344fa9968c8f
Dancharike <145747723+Dancharike@users.noreply.github.com>
2025-10-28 18:31:59 +0200

    Initial commit

---
+++ BerestaTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BerestaTest;
+
+class Program
+{
+    [DllImport("libBerestaCore.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, EntryPoint = "run_script")]
+    public static extern IntPtr RunScript(string code);
+    
+    private static void Main()
+    {
+        string script = "console_print(\"Hello from C# via BerestaCore!\");";
+        
+        IntPtr resultPtr = RunScript(script);
+        
+        string result = Marshal.PtrToStringAnsi(resultPtr) ?? "<null>";
+        
+        Console.WriteLine("DLL returned: ");
+        Console.WriteLine(result);
+    }
+}
